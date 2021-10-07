c0b2f7f60854c2ce2216c0d40273d187f65dd372
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-07 13:16:27 -0400

    🚧 Add placeholder methods

--- Sources/Appcues/Appcues.swift
+++ Sources/Appcues/Appcues.swift
@@ -9,4 +9,22 @@
 import Foundation
 
 public class Appcues {
+
+    /// Temporary internal log of API calls
+    var log: [String] = []
+
+    public init() {
+    }
+
+    public func identify(userId: String, traits: [String: String]) {
+        log.append("Appcues.identify(userId: \(userId))")
+    }
+
+    public func track(event: String, properties: [String: String]) {
+        log.append("Appcues.track(event: \(event))")
+    }
+
+    public func screen(title: String, properties: [String: String]) {
+        log.append("Appcues.screen(title: \(title))")
+    }
 }
