e29c378d48e4e9e357216510952e6a618a096c74
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-07 13:16:15 -0400

    ✨ Add initial SDK scaffold

---
+++ Sources/Appcues/Appcues.swift
@@ -0,0 +1,12 @@
+//
+//  Appcues.swift
+//  Appcues
+//
+//  Created by Matt on 2021-10-06.
+//  Copyright © 2021 Appcues. All rights reserved.
+//
+
+import Foundation
+
+public class Appcues {
+}
---
+++ Tests/AppcuesTests/AppcuesTests.swift
@@ -0,0 +1,18 @@
+//
+//  AppcuesTests.swift
+//  AppcuesTests
+//
+//  Created by Matt on 2021-10-06.
+//  Copyright © 2021 Appcues. All rights reserved.
+//
+
+import XCTest
+
+class AppcuesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
