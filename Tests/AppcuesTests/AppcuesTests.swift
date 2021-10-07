ddba555e80bbbb5a2ff3027f83e6d8051b164668
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-07 13:17:06 -0400

    ✅ Add tests for placeholder SDK methods

--- Tests/AppcuesTests/AppcuesTests.swift
+++ Tests/AppcuesTests/AppcuesTests.swift
@@ -7,12 +7,34 @@
 //
 
 import XCTest
+@testable import Appcues
 
 class AppcuesTests: XCTestCase {
 
+    var instance: Appcues!
+
     override func setUpWithError() throws {
+        instance = Appcues()
     }
 
     override func tearDownWithError() throws {
+    }
+
+    func testIdentify() throws {
+        instance.identify(userId: "abc", traits: [:])
+        XCTAssertEqual(instance.log.count, 1)
+        XCTAssertEqual(instance.log.first, "Appcues.identify(userId: abc)")
+    }
+
+    func testTrackEvent() throws {
+        instance.track(event: "eventName", properties: [:])
+        XCTAssertEqual(instance.log.count, 1)
+        XCTAssertEqual(instance.log.first, "Appcues.track(event: eventName)")
+    }
+
+    func testTrackScreen() throws {
+        instance.screen(title: "Homescreen", properties: [:])
+        XCTAssertEqual(instance.log.count, 1)
+        XCTAssertEqual(instance.log.first, "Appcues.screen(title: Homescreen)")
     }
 }
