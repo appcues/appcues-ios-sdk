315febc5a933e6fd037b5bbf4c91e89717d316e7
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-08 11:02:07 -0400

    💡 Add docs for Appcues interface

--- Sources/Appcues/Appcues.swift
+++ Sources/Appcues/Appcues.swift
@@ -8,22 +8,36 @@
 
 import Foundation
 
+/// An object that manages Appcues tracking for your app.
 public class Appcues {
 
     /// Temporary internal log of API calls
     var log: [String] = []
 
+    /// Creates an instance of Appcues analytics.
     public init() {
     }
 
+    /// Identify the user and determine if they should see Appcues content.
+    /// - Parameters:
+    ///   - userId: Unique value identifying the user.
+    ///   - traits: Optional properties that provide additional context about the user.
     public func identify(userId: String, traits: [String: String]) {
         log.append("Appcues.identify(userId: \(userId))")
     }
 
+    /// Track an action taken by a user.
+    /// - Parameters:
+    ///   - event: Name of the event.
+    ///   - properties: Optional properties that provide additional context about the event.
     public func track(event: String, properties: [String: String]) {
         log.append("Appcues.track(event: \(event))")
     }
 
+    /// Track an screen viewed by a user.
+    /// - Parameters:
+    ///   - title: Name of the screen.
+    ///   - properties: Optional properties that provide additional context about the event.
     public func screen(title: String, properties: [String: String]) {
         log.append("Appcues.screen(title: \(title))")
     }
