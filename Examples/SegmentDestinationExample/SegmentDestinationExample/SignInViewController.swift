0a9c0fe135be481441f03ca21d67f23cb67636c0
James Ellis <james@appcues.com>
2021-10-14 14:26:11 -0400

    🎬 Add segment integration example app

---
+++ Examples/SegmentDestinationExample/SegmentDestinationExample/SignInViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignInViewController.swift
+//  SegmentDestinationExample
+//
+//  Created by James Ellis on 10/13/21.
+//
+
+import UIKit
+import Segment
+
+enum User {
+    static var currentID = "default-00000"
+}
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet private var userIDTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        userIDTextField.text = User.currentID
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        Analytics.shared.screen(title: "Sign In")
+    }
+
+    @IBAction private func signInTapped(_ sender: UIButton) {
+        let userID = userIDTextField.text ?? User.currentID
+        Analytics.shared.identify(userId: userID)
+
+        User.currentID = userID
+    }
+}
