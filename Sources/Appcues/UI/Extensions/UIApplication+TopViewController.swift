c2e59b3fae19726a1f9795de14ddd74f9f4f9968
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-21 12:15:56 -0400

    ♻️ Improve topViewController mapping

--- Sources/Appcues/UI/Extensions/UIApplication+TopViewController.swift
+++ Sources/Appcues/UI/Extensions/UIApplication+TopViewController.swift
@@ -10,24 +10,37 @@ import UIKit
 
 extension UIApplication {
 
-    func topViewController() -> UIViewController? {
-        let window = self.connectedScenes
+    // Note: multitasking with two instances of the same app side by side will have both designated as `.foregroundActive`,
+    // and as a result the returned window may not be the one expected.
+    private var activeKeyWindow: UIWindow? {
+        self.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
             .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
             .first { $0.isKeyWindow }
-
-        return topViewController(controller: window?.rootViewController)
     }
 
-    private func topViewController(controller: UIViewController?) -> UIViewController? {
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
+    func topViewController() -> UIViewController? {
+        guard let rootViewController = activeKeyWindow?.rootViewController else { return nil }
+        return topViewController(controller: rootViewController)
+    }
+
+    private func topViewController(controller: UIViewController) -> UIViewController {
+        if let navigationController = controller as? UINavigationController,
+           let visibleViewController = navigationController.visibleViewController {
+            if !visibleViewController.isBeingDismissed {
+                return topViewController(controller: visibleViewController)
+            } else if let topStack = navigationController.viewControllers.last {
+                // This gets the VC under what is being dismissed
+                return topViewController(controller: topStack)
+            } else {
+                return topViewController(controller: visibleViewController)
             }
         }
-        if let presented = controller?.presentedViewController {
+        if let tabController = controller as? UITabBarController,
+           let selected = tabController.selectedViewController {
+            return topViewController(controller: selected)
+        }
+        if let presented = controller.presentedViewController, !presented.isBeingDismissed {
             return topViewController(controller: presented)
         }
         return controller
