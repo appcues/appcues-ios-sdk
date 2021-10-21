54cbe4334807319f24a8ad805bfae64d4e06980f
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-21 16:45:40 -0400

    ♻️ Update property dictionaries to allow any type
    Unsupported types will be ignored

---
+++ Sources/AppcuesKit/Networking/DynamicCodingKeys.swift
@@ -0,0 +1,58 @@
+//
+//  DynamicCodingKeys.swift
+//  AppcuesKit
+//
+//  Created by Matt on 2021-10-21.
+//  Copyright © 2021 Appcues. All rights reserved.
+//
+
+import Foundation
+
+internal struct DynamicCodingKeys: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    init?(intValue: Int) {
+        // No int keys supported
+        return nil
+    }
+
+    // Non-failable init for encoding
+    init(key: String) {
+        stringValue = key
+    }
+}
+
+extension KeyedEncodingContainer where K == DynamicCodingKeys {
+
+    /// Encodes the given dictionary to primitive types permitted by the Appcues API.
+    ///
+    /// An `EncodingError` will be thrown for non-permitted types, and that specific error should be caught and ignored by the caller of this func.
+    mutating func encode(_ dict: [String: Any]?) throws {
+        try dict?.forEach { key, value in
+            let codingKey = DynamicCodingKeys(key: key)
+
+            switch value {
+            case let string as String:
+                try self.encode(string, forKey: codingKey)
+            case let int as Int:
+                try self.encode(int, forKey: codingKey)
+            case let double as Double:
+                try self.encode(double, forKey: codingKey)
+            case let bool as Bool:
+                try self.encode(bool, forKey: codingKey)
+            default:
+                // Throw here instead of assertionFailure directly so we can test this case
+                let context = EncodingError.Context(
+                    codingPath: [codingKey],
+                    debugDescription: "Only String, Number and Bool types allowed.",
+                    underlyingError: AppcuesEncodingError.unsupportedType)
+                throw EncodingError.invalidValue(value, context)
+            }
+        }
+    }
+}
