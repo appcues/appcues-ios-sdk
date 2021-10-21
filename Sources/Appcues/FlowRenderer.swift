c9c7d8713253d757b49fe1638ff01fe1de88a22d
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-21 10:53:18 -0400

    ♻️ Update main thread dispatching

--- Sources/Appcues/FlowRenderer.swift
+++ Sources/Appcues/FlowRenderer.swift
@@ -20,13 +20,13 @@ internal class FlowRenderer {
 
     // Show a specified flow model on top of the current application.
     func show(flow: Flow) {
-        DispatchQueue.main.async {
-            guard let modalStepGroup: ModalGroup = flow.steps.compactMap({ $0 as? ModalGroup }).first else {
-                // Currently only supporting a single ModalGroup. Additional modal groups or other types aren't supported yet.
-                self.config.logger.error("Cannot show flow %{public}s because it has no modal groups", flow.id)
-                return
-            }
+        guard let modalStepGroup: ModalGroup = flow.steps.compactMap({ $0 as? ModalGroup }).first else {
+            // Currently only supporting a single ModalGroup. Additional modal groups or other types aren't supported yet.
+            self.config.logger.error("Cannot show flow %{public}s because it has no modal groups", flow.id)
+            return
+        }
 
+        DispatchQueue.main.async {
             guard let topController = UIApplication.shared.topViewController() else {
                 self.config.logger.error("Could not determine top view controller")
                 return
