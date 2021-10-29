1cd29d2ee319f8cf0d796f904c512beed7f24795
Matt Hayashida <matt.hayashida@appcues.com>
2021-10-29 10:20:33 -0400

    🎬 Add SPM integration example app

---
+++ Examples/DeveloperSPMExample/SPMExample/EventsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EventsViewController.swift
+//  AppcuesExample
+//
+//  Created by Matt on 2021-10-12.
+//
+
+import UIKit
+import AppcuesKit
+
+class EventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        Appcues.shared.screen(title: "Trigger Events")
+    }
+
+    @IBAction private func buttonOneTapped(_ sender: UIButton) {
+        Appcues.shared.track(name: "event1")
+    }
+
+    @IBAction private func buttonTwoTapped(_ sender: UIButton) {
+        Appcues.shared.track(name: "event2")
+    }
+}
