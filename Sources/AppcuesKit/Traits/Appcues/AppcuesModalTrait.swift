70dec494100c251b5907a95cbc8d6d25eaba41ff
Matt Hayashida <matt.hayashida@appcues.com>
2021-11-04 15:04:28 -0400

    ✨ Add optional cornerRadius config to AppcuesModalTrait

--- Sources/AppcuesKit/Traits/Appcues/AppcuesModalTrait.swift
+++ Sources/AppcuesKit/Traits/Appcues/AppcuesModalTrait.swift
@@ -12,6 +12,7 @@ internal struct AppcuesModalTrait: ContainerExperienceTrait {
     static let type = "@appcues/modal"
 
     var presentationStyle: PresentationStyle
+    var cornerRadius: CGFloat?
 
     init?(config: [String: Any]?) {
         if let presentationStyle = PresentationStyle(rawValue: config?["presentationStyle"] as? String ?? "") {
@@ -19,19 +20,30 @@ internal struct AppcuesModalTrait: ContainerExperienceTrait {
         } else {
             return nil
         }
+
+        if let cornerRadius = config?["cornerRadius"] as? Int {
+            self.cornerRadius = CGFloat(cornerRadius)
+        }
     }
 
     func apply(to viewController: UIViewController) -> UIViewController {
         viewController.modalPresentationStyle = presentationStyle.modalPresentationStyle
 
         if presentationStyle == .dialog {
-            return DialogContainerViewController(dialogViewController: viewController)
+            let dialogViewController = DialogContainerViewController(dialogViewController: viewController)
+            if let cornerRadius = cornerRadius {
+                dialogViewController.containerView.dialogView.layer.cornerRadius = cornerRadius
+            }
+            return dialogViewController
         }
 
-        if #available(iOS 15.0, *), presentationStyle == .halfSheet, let sheet = viewController.sheetPresentationController {
-            sheet.detents = [.medium(), .large()]
-            sheet.prefersGrabberVisible = true
+        if #available(iOS 15.0, *), let sheet = viewController.sheetPresentationController {
+            sheet.preferredCornerRadius = cornerRadius
 
+            if presentationStyle == .halfSheet {
+                sheet.detents = [.medium(), .large()]
+                sheet.prefersGrabberVisible = true
+            }
         }
 
         return viewController
