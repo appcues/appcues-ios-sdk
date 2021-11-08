ea3a7b6252edbbae7ad420faba60d924d4de4cd3
Matt Hayashida <matt.hayashida@appcues.com>
2021-11-08 15:27:44 -0500

    ♻️ Update Color init to RGBA

--- Sources/AppcuesKit/UI/Extensions/Inits/Color+Hex.swift
+++ Sources/AppcuesKit/UI/Extensions/Inits/Color+Hex.swift
@@ -20,13 +20,13 @@ extension Color {
         let a, r, g, b: UInt64
         switch hex.count {
         case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            (r, g, b, a) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17, 255)
         case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            (r, g, b, a) = (int >> 16, int >> 8 & 0xFF, int & 0xFF, 255)
+        case 8: // RGBA (32-bit)
+            (r, g, b, a) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
         default:
-            (a, r, g, b) = (1, 1, 1, 0)
+            (r, g, b, a) = (0, 0, 0, 255)
         }
 
         self.init(
