0fef36515d84558b3278d559f9e47264b3dfc096
James Ellis <james@appcues.com>
2021-11-09 14:19:33 -0500

    👌 Simplify the Debugger hit test logic

--- Sources/AppcuesKit/Debugger/FloatingView/DebugUIWindow.swift
+++ Sources/AppcuesKit/Debugger/FloatingView/DebugUIWindow.swift
@@ -24,10 +24,8 @@ internal class DebugUIWindow: UIWindow {
     }
 
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        guard let hitView = super.hitTest(point, with: event) else { return nil }
-
-        if hitView == self {
-            return rootViewController?.view.hitTest(point, with: event)
+        guard let hitView = super.hitTest(point, with: event), hitView != self else {
+            return nil
         }
 
         return hitView
