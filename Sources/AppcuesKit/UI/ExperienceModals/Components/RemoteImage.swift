37c574ae86da1a328ab994ddbfc11f061b5ae03e
Matt Hayashida <matt.hayashida@appcues.com>
2021-11-10 13:36:40 -0500

    ✨ Add SessionImageCache for experience images

--- Sources/AppcuesKit/UI/ExperienceModals/Components/RemoteImage.swift
+++ Sources/AppcuesKit/UI/ExperienceModals/Components/RemoteImage.swift
@@ -12,21 +12,35 @@ import Combine
 // AsyncImage is iOS 15+, so borrow this from
 // https://www.vadimbulavin.com/asynchronous-swiftui-image-loading-from-url-with-combine-and-swift/
 
-// TODO: Cache remote images for performance benefits.
 internal class ImageLoader: ObservableObject {
     @Published var image: UIImage?
+    private(set) var isLoading = false
     private let url: URL
+
+    private var cache: SessionImageCache?
 
     private var cancellable: AnyCancellable?
 
-    init(url: URL) {
+    init(url: URL, cache: SessionImageCache? = nil) {
         self.url = url
+        self.cache = cache
     }
 
     func load() {
+        guard !isLoading else { return }
+
+        if let image = cache?[url] {
+            self.image = image
+            return
+        }
+
         cancellable = URLSession.shared.dataTaskPublisher(for: url)
             .map { UIImage(data: $0.data) }
             .replaceError(with: nil)
+            .handleEvents(receiveSubscription: { [weak self] _ in self?.isLoading = true },
+                          receiveOutput: { [weak self, url] in self?.cache?[url] = $0 },
+                          receiveCompletion: { [weak self] _ in self?.isLoading = false },
+                          receiveCancel: { [weak self] in self?.isLoading = false })
             .receive(on: DispatchQueue.main)
             .sink { [weak self] in self?.image = $0 }
     }
@@ -62,6 +76,6 @@ internal struct RemoteImage<Placeholder: View>: View {
 
     init(url: URL, @ViewBuilder placeholder: () -> Placeholder) {
         self.placeholder = placeholder()
-        loader = ImageLoader(url: url)
+        loader = ImageLoader(url: url, cache: Environment(\.imageCache).wrappedValue)
     }
 }
