a6601fc3c106ba95ad69d9b55a163fe3606195cf
James Ellis <james@appcues.com>
2021-11-22 14:32:33 -0500

    🎬 Update cocoapod example to remove lifecycle event tracking call

--- Examples/DeveloperCocoapodsExample/CocoapodsExample/AppDelegate.swift
+++ Examples/DeveloperCocoapodsExample/CocoapodsExample/AppDelegate.swift
@@ -15,10 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        // to opt-in for standard application lifecycle events, the SDK must be
-        // initialzed and have its trackLifecycle() function called during app startup here
-        Appcues.shared.trackLifecycle()
-
         return true
     }
 
