17d2955ee24caa55b955f5a010035bcc637f5cc6
James Ellis <james@appcues.com>
2021-12-08 16:34:52 -0500

    ♻️ Update deeplink scheme to be based on application ID rather than account ID

--- fastlane/replace-placeholders.sh
+++ fastlane/replace-placeholders.sh
@@ -1,4 +1,4 @@
 # replace placeholders
 sed -i '' -e "s/<#APPCUES_ACCOUNT_ID#>/\"$2\"/g" ../Examples/${1}/AppDelegate.swift
-sed -i '' -e "s/APPCUES_ACCOUNT_ID/$2/g" ../Examples/${1}/Info.plist
 sed -i '' -e "s/<#APPCUES_APPLICATION_ID#>/\"$3\"/g" ../Examples/${1}/AppDelegate.swift
+sed -i '' -e "s/APPCUES_APPLICATION_ID/$2/g" ../Examples/${1}/Info.plist
