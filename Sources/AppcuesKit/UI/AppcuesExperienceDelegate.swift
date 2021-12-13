6934a99f69e2d9d2a2ced1e777e0f05b3d09b694
Matt Hayashida <matt.hayashida@appcues.com>
2021-12-13 13:50:08 -0500

    💡 Add documentation for AppcuesExperienceDelegate

--- Sources/AppcuesKit/UI/AppcuesExperienceDelegate.swift
+++ Sources/AppcuesKit/UI/AppcuesExperienceDelegate.swift
@@ -8,11 +8,19 @@
 
 import Foundation
 
+/// A set of methods that allow you to control and respond to Appcues experiences being displayed changes in your app.
+
 public protocol AppcuesExperienceDelegate: AnyObject {
+    /// Asks the delegate for permission to display the Appcues experience.
+    /// - Returns: `true` to allow the SDK to display the experience, `false` to refuse the presentaiton.
     func canDisplayExperience(experienceID: String) -> Bool
+    /// Notifies the delegate before an Appcues experience is presented.
     func experienceWillAppear()
+    /// Notifies the delegate after an Appcues experience is presented.
     func experienceDidAppear()
+    /// Notifies the delegate before an Appcues experience is dismissed.
     func experienceWillDisappear()
+    /// Notifies the delegate after an Appcues experience is dismissed.
     func experienceDidDisappear()
 }
 
