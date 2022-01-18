3de9943987364f2fdedd18a49ddcebb13b880604
Matt Hayashida <matt.hayashida@appcues.com>
2022-01-18 11:08:27 -0500

    ♻️ Update ExperiencePagingViewController to work with the sticky content trait

--- Sources/AppcuesKit/UI/ExperienceModals/ExperienceStepViewController.swift
+++ Sources/AppcuesKit/UI/ExperienceModals/ExperienceStepViewController.swift
@@ -12,6 +12,8 @@ internal class ExperienceStepViewController: UIViewController {
 
     let viewModel: ExperienceStepViewModel
 
+    lazy var stepView = ExperienceStepView()
+
     private let contentViewController: UIViewController
 
     init(viewModel: ExperienceStepViewModel) {
@@ -29,9 +31,43 @@ internal class ExperienceStepViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func loadView() {
+        view = stepView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        embedChildViewController(contentViewController, inSuperview: view)
+        embedChildViewController(contentViewController, inSuperview: stepView.scrollView)
+        NSLayoutConstraint.activate([
+            contentViewController.view.widthAnchor.constraint(equalTo: stepView.scrollView.widthAnchor)
+        ])
+    }
+}
+
+extension ExperienceStepViewController {
+    class ExperienceStepView: UIView {
+        lazy var scrollView: UIScrollView = {
+            let view = UIScrollView()
+            // Force a consistent safe area behaviour regardless of whether the content scrolls
+            view.contentInsetAdjustmentBehavior = .always
+            return view
+        }()
+
+        var contentHeight: CGFloat {
+            scrollView.contentSize.height
+        }
+
+        init() {
+            super.init(frame: .zero)
+
+            addSubview(scrollView)
+            scrollView.pin(to: self)
+        }
+
+        @available(*, unavailable)
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
     }
 }
