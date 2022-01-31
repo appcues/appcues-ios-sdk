1495c7f398133bf95c7098b19ab03d9e87521a78
Matt Hayashida <matt.hayashida@appcues.com>
2022-01-31 15:13:56 -0500

    ♻️ Remove default container/presenting trait behaviors

--- Sources/AppcuesKit/Traits/Appcues/AppcuesModalTrait.swift
+++ Sources/AppcuesKit/Traits/Appcues/AppcuesModalTrait.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-internal struct AppcuesModalTrait: WrapperCreatingTrait {
+internal struct AppcuesModalTrait: ContainerCreatingTrait, WrapperCreatingTrait, PresentingTrait {
     static let type = "@appcues/modal"
 
     let modalConfig: ModalConfig
@@ -21,11 +21,23 @@ internal struct AppcuesModalTrait: WrapperCreatingTrait {
         }
     }
 
+    func createContainer(for stepControllers: [UIViewController], targetPageIndex: Int) throws -> ExperienceStepContainer {
+        ExperiencePagingViewController(stepControllers: stepControllers, groupID: nil)
+    }
+
     func createWrapper(around containerController: ExperienceStepContainer) -> UIViewController {
         return modalConfig.createWrapper(around: containerController)
     }
 
     func addBackdrop(backdropView: UIView, to wrapperController: UIViewController) {
         modalConfig.addBackdrop(backdropView: backdropView, to: wrapperController)
+    }
+
+    func present(viewController: UIViewController) throws {
+        UIApplication.shared.topViewController()?.present(viewController, animated: true)
+    }
+
+    func remove(viewController: UIViewController) {
+        viewController.dismiss(animated: true)
     }
 }
