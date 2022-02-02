29c380de2fd4011e7ff1ec75c060fff93054b3d5
Matt Hayashida <matt.hayashida@appcues.com>
2022-02-02 15:32:15 -0500

    💡 Add inline docs for Trait API

--- Sources/AppcuesKit/Traits/ExperienceTrait.swift
+++ Sources/AppcuesKit/Traits/ExperienceTrait.swift
@@ -15,9 +15,17 @@ public protocol ExperienceTrait {
 
     /// The name of the trait.
     ///
-    /// Should be formatted `@org/name` (e.g. `@appcues/modal`).
+    /// Must be unique and should be formatted `@org/name` (e.g. `@appcues/modal`).
     static var type: String { get }
 
+    /// `@appcues/group` ID reference for experience-level traits.
+    ///
+    /// This value is ignored for step-level traits.
+    ///
+    /// Any trait wishing to be groupable as an experience-level trait **must** map this value in its ``ExperienceTrait/init(config:)`` with
+    /// ```swift
+    /// self.groupID = config?["groupID"] as? String
+    /// ```
     var groupID: String? { get }
 
     /// Initializer from an `Experience.Trait` data model.
@@ -26,32 +34,90 @@ public protocol ExperienceTrait {
     init?(config: [String: Any]?)
 }
 
- internal protocol GroupingTrait: ExperienceTrait {
+internal protocol GroupingTrait: ExperienceTrait {
     func join(initialStep stepIndex: Int, in experience: Experience) -> [Experience.Step]
 }
 
+/// A trait that modifies the `UIViewController` that encapsulates the contents of a specific step in the experience.
 public protocol StepDecoratingTrait: ExperienceTrait {
+
+    /// Modify the view controller for a step.
+    /// - Parameter stepController: The `UIViewController` to modify.
+    ///
+    /// If this method cannot properly apply the trait behavior, it may throw an error of type ``TraitError``,
+    /// ending the attempt to display the experience.
     func decorate(stepController: UIViewController) throws
 }
 
+/// A trait responsible for creating the `UIViewController` (specifically a ``ExperienceStepContainer``) that holds the experience step(s)
+/// being presented. The returned controller must call the ``ExperienceContainerLifecycleHandler`` methods at the appropriate times.
 public protocol ContainerCreatingTrait: ExperienceTrait {
+
+    /// Create the container controller for experience step(s).
+    /// - Parameter stepControllers: Array of controllers being presented.
+    /// - Parameter targetPageIndex: The index in the `stepControllers` that should be displayed with primary focus.
+    /// - Returns: A `UIViewController` that contains the `stepControllers` as children.
+    ///
+    /// `stepControllers` is guaranteed to be non-empty and may frequently have only a single item.
+    ///
+    /// If this method cannot properly apply the trait behavior, it may throw an error of type ``TraitError``,
+    /// ending the attempt to display the experience.
     func createContainer(for stepControllers: [UIViewController], targetPageIndex: Int) throws -> ExperienceStepContainer
 }
 
+/// A trait that modifies the container view controller created by an ``ContainerCreatingTrait``.
 public protocol ContainerDecoratingTrait: ExperienceTrait {
+
+    /// Modify a container view controller.
+    /// - Parameter containerController: The `UIViewController` to modify.
+    ///
+    /// If this method cannot properly apply the trait behavior, it may throw an error of type ``TraitError``,
+    /// ending the attempt to display the experience.
     func decorate(containerController: ExperienceStepContainer) throws
 }
 
+/// A trait that modifies the backdrop `UIView` that may be included in the presented experience.
 public protocol BackdropDecoratingTrait: ExperienceTrait {
+
+    /// Modify the backdrop view.
+    /// - Parameter backdropView: The `UIView` to modify.
+    ///
+    /// Prefer adding a subview to `backdropView` over modifying `backdropView` itself where possible
+    /// to preserve composability of multiple traits.
+    ///
+    /// If this method cannot properly apply the trait behavior, it may throw an error of type ``TraitError``,
+    /// ending the attempt to display the experience.
     func decorate(backdropView: UIView) throws
 }
 
+/// A trait that creates a `UIViewController` that wraps the ``ExperienceStepContainer``.
 public protocol WrapperCreatingTrait: ExperienceTrait {
+
+    /// Create a wrapper controller around a container controller.
+    /// - Parameter containerController: The container controller.
+    /// - Returns: A `UIViewController` that contains the `containerController` as a child.
+    ///
+    /// If this method cannot properly apply the trait behavior, it may throw an error of type ``TraitError``,
+    /// ending the attempt to display the experience.
     func createWrapper(around containerController: ExperienceStepContainer) throws -> UIViewController
+
+    /// Add the decorated backdrop view to the wrapper.
+    /// - Parameter backdropView: The backdrop.
+    /// - Parameter wrapperController: The wrapper view controller.
     func addBackdrop(backdropView: UIView, to wrapperController: UIViewController)
 }
 
+/// A trait responsible for providing the ability to show and hide the experience.
 public protocol PresentingTrait: ExperienceTrait {
+
+    /// Create a wrapper controller around a container controller.
+    /// - Parameter viewController: The view controller to .
+    ///
+    /// If this method cannot properly apply the trait behavior, it may throw an error of type ``TraitError``,
+    /// ending the attempt to display the experience.
     func present(viewController: UIViewController) throws
+
+    /// Create a wrapper controller around a container controller.
+    /// - Parameter viewController: The view controller to remove.
     func remove(viewController: UIViewController)
 }
--- Sources/AppcuesKit/UI/ExperienceStepContainer.swift
+++ Sources/AppcuesKit/UI/ExperienceStepContainer.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
+/// A `UIViewController` which contains step view controllers to display an `Experience`.
 public protocol ExperienceStepContainer: UIViewController {
+
+    /// The delegate object for the experience step container.
     var lifecycleHandler: ExperienceContainerLifecycleHandler? { get set }
 
+    /// Update the step controller in focus.
+    /// - Parameter pageIndex: The index of the controller to navigate to.
+    ///
+    /// The implementation of this method should result in
+    /// ``ExperienceContainerLifecycleHandler/containerNavigated(from:to:)`` being called.
     func navigate(to pageIndex: Int)
 }
