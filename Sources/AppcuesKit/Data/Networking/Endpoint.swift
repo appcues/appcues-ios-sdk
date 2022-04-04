06fa3d26b0cf2887e77047519a5e780c799b88a2
James Ellis <james@appcues.com>
2022-04-04 15:05:49 -0400

    ♻️ Allow userID to be included on experience_preview endpoint

--- Sources/AppcuesKit/Data/Networking/Endpoint.swift
+++ Sources/AppcuesKit/Data/Networking/Endpoint.swift
@@ -27,26 +27,12 @@ internal enum APIEndpoint: Endpoint {
         case let .content(experienceID):
             components.path = "/v1/accounts/\(config.accountID)/users/\(storage.userID)/experience_content/\(experienceID)"
         case let .preview(experienceID):
-            // note: preview does not contain the /users/{user_id} portion of the path
-            components.path = "/v1/accounts/\(config.accountID)/experience_preview/\(experienceID)"
-        }
-
-        return components.url
-    }
-}
-
-/// Endpoint in the Appcues CDN.
-internal enum CDNEndpoint: Endpoint {
-    case styles(styleID: String)
-
-    func url(config: Appcues.Config, storage: DataStoring) -> URL? {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "fast.appcues.com"
-
-        switch self {
-        case let .styles(styleID):
-            components.path = "/v1/accounts/\(config.accountID)/styles/\(styleID)"
+            // optionally include the userID, if one exists, to allow for a personalized preview capability
+            if storage.userID.isEmpty {
+                components.path = "/v1/accounts/\(config.accountID)/experience_preview/\(experienceID)"
+            } else {
+                components.path = "/v1/accounts/\(config.accountID)/users/\(storage.userID)/experience_preview/\(experienceID)"
+            }
         }
 
         return components.url
