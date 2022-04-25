5e380ff1786bab1aa420e7d90408d6cff2859616
Matt Hayashida <matt.hayashida@appcues.com>
2022-04-25 09:08:04 -0400

    ✨ Convert svg image urls to scaled png urls

--- Sources/AppcuesKit/Presentation/UI/Components/AppcuesImage.swift
+++ Sources/AppcuesKit/Presentation/UI/Components/AppcuesImage.swift
@@ -11,6 +11,7 @@ import SwiftUI
 @available(iOS 13.0, *)
 internal struct AppcuesImage: View {
     let model: ExperienceComponent.ImageModel
+    private let transformedURL: URL
 
     @EnvironmentObject var viewModel: ExperienceStepViewModel
     @Environment(\.imageCache) var imageCache: SessionImageCache
@@ -36,12 +37,19 @@ internal struct AppcuesImage: View {
             .applyExternalLayout(style)
     }
 
+    internal init(model: ExperienceComponent.ImageModel) {
+        self.model = model
+
+        // Setting this in the init to avoid recomputing it more frequently than necessary.
+        self.transformedURL = model.imageUrl.transformImage(width: model.style?.width, height: model.style?.height)
+    }
+
     @ViewBuilder
     private func content(placeholder: Color?) -> some View {
         if model.imageUrl.scheme == "sf-symbol" {
             Image(systemName: model.imageUrl.host ?? "")
         } else {
-            RemoteImage(url: model.imageUrl, cache: imageCache) {
+            RemoteImage(url: transformedURL, cache: imageCache) {
                 if let blurImage = Image(blurHash: model.blurHash) {
                     blurImage.resizable()
                 } else {
@@ -49,6 +57,50 @@ internal struct AppcuesImage: View {
                 }
             }
         }
+    }
+}
+
+private extension URL {
+
+    /// Transforms an image URL to one with the best chance of rendering.
+    func transformImage(width: Double? = nil, height: Double? = nil) -> URL {
+        // Note: we're not specifically checking for Cloudinary in the URL because there's no functional
+        // difference between a non-renderable image and a 404 caused by transforming a non-Cloudinary URL.
+        if pathExtension == "svg" {
+            return transformSVG(components: URLComponents(url: self, resolvingAgainstBaseURL: false), width: width, height: height) ?? self
+        }
+
+        return self
+    }
+
+    /// Converts a URL like https://res.cloudinary.com/xyz/image/upload/v123/000/abc.svg to
+    /// https://res.cloudinary.com/xyz/image/upload/h_600,c_scale/v123/000/abc.png
+    private func transformSVG(components: URLComponents?, width: Double? = nil, height: Double? = nil) -> URL? {
+        guard var components = components else { return self }
+
+        // Replace .svg in the path extension with .png
+        let options: String.CompareOptions = [.backwards, .caseInsensitive]
+        guard let range = components.path.range(of: ".svg", options: options, range: nil, locale: nil) else { return self }
+        components.path = components.path.replacingCharacters(in: range, with: ".png")
+
+        // Scale the png to the right size so it appears crisp like a vector would.
+        let scale: CGFloat = UIScreen.main.scale
+        var parts = components.path.split(separator: "/")
+        if parts.count == 6, parts[2] == "upload" {
+            if let width = width {
+                // An integer scale value represents a fixed pixel size.
+                parts.insert("w_\(Int(width * scale)),c_scale", at: 3)
+            } else if let height = height {
+                parts.insert("h_\(Int(height * scale)),c_scale", at: 3)
+            } else {
+                // If no fixed size, scale the default SVG size by the screen density.
+                // A decimal scale value represents a percentage.
+                parts.insert("w_\(scale),c_scale", at: 3)
+            }
+        }
+        components.path = "/" + parts.joined(separator: "/")
+
+        return components.url
     }
 }
 
