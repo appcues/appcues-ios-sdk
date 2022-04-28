d8b2a515dd1b5137a05b05613a75dce457423db3
Matt Hayashida <matt.hayashida@appcues.com>
2022-04-28 13:39:47 -0400

    ♻️ Remove fontWeight experience property and update debugger font listing

--- Sources/AppcuesKit/Presentation/Debugger/Panel/DebugFontUI.swift
+++ Sources/AppcuesKit/Presentation/Debugger/Panel/DebugFontUI.swift
@@ -8,47 +8,69 @@
 
 import SwiftUI
 
-@available(iOS 13.0, *)
-private struct FontInfo {
-    struct VariableInfo {
-        let range: ClosedRange<Double>
-        let initial: Double
-    }
-
-    let name: String
-    let variableInfo: VariableInfo?
-
-    init(name: String, variableInfo: FontInfo.VariableInfo? = nil) {
-        self.name = name
-        self.variableInfo = variableInfo
-    }
-}
-
 /// Namespaced Views used in the debug panel.
 @available(iOS 13.0, *)
 internal enum DebugFontUI {
+    enum FontInfo {
+        static func allFonts() -> [String] {
+            UIFont.familyNames.flatMap {
+                UIFont.fontNames(forFamilyName: $0)
+            }
+        }
+
+        static func systemFonts() -> [String] {
+            Font.Design.allCases.flatMap { design in
+                Font.Weight.allCases.map { weight in
+                    "System \(design.description) \(weight.description)"
+                }
+            }
+        }
+
+        static func appFonts() -> [String] {
+            let familyNames: [String] = (Bundle.main.infoDictionary?["UIAppFonts"] as? [String] ?? [])
+                .compactMap { resourceName in
+                    if let url = Bundle.main.url(forResource: resourceName, withExtension: nil),
+                       let fontData = try? Data(contentsOf: url),
+                       let fontDataProvider = CGDataProvider(data: fontData as CFData),
+                       let font = CGFont(fontDataProvider),
+                       let name = font.postScriptName as? String {
+                        let ctfont = CTFontCreateWithName(name as CFString, 17, nil)
+                        return CTFontCopyFamilyName(ctfont) as String
+                    } else {
+                        return nil
+                    }
+                }
+
+            return Set(familyNames)
+                .sorted()
+                .flatMap {
+                    UIFont.fontNames(forFamilyName: $0)
+                }
+        }
+    }
+
     struct FontListView: View {
-        private let appFonts: [FontInfo] = FontInfo.appFonts()
-        private let systemFonts: [FontInfo] = FontInfo.systemFonts()
-        private let allFonts: [FontInfo] = FontInfo.allFonts()
+        private let appFonts = FontInfo.appFonts()
+        private let systemFonts = FontInfo.systemFonts()
+        private let allFonts = FontInfo.allFonts()
 
         var body: some View {
             List {
                 Section(header: Text("App-Specific Fonts")) {
-                    ForEach(appFonts, id: \.name) {
-                        FontItem(fontInfo: $0)
+                    ForEach(appFonts, id: \.self) {
+                        FontItem(name: $0)
                     }
                 }
 
                 Section(header: Text("System Fonts")) {
-                    ForEach(systemFonts, id: \.name) {
-                        FontItem(fontInfo: $0)
+                    ForEach(systemFonts, id: \.self) {
+                        FontItem(name: $0)
                     }
                 }
 
                 Section(header: Text("All Fonts")) {
-                    ForEach(allFonts, id: \.name) {
-                        FontItem(fontInfo: $0)
+                    ForEach(allFonts, id: \.self) {
+                        FontItem(name: $0)
                     }
                 }
             }
@@ -57,74 +79,21 @@ internal enum DebugFontUI {
     }
 
     private struct FontItem: View {
-        let fontInfo: FontInfo
-
-        @State private var weight: Double
+        let name: String
 
         var body: some View {
-            VStack {
-                HStack {
-                    if fontInfo.variableInfo != nil {
-                        Text("\(fontInfo.name) #\(Int(weight))")
-                            .font(Font.custom(fontInfo.name, size: UIFont.labelFontSize, weight: weight))
-                    } else {
-                        Text(fontInfo.name)
-                            .font(Font(name: fontInfo.name, size: UIFont.labelFontSize, weight: nil))
-                    }
-                    Spacer()
-                    Button {
-                        if fontInfo.variableInfo != nil {
-                            UIPasteboard.general.string = "\(fontInfo.name) #\(Int(weight))"
-                        } else {
-                            UIPasteboard.general.string = fontInfo.name
-
-                        }
-                    } label: {
-                        Image(systemName: "doc.on.doc")
-                            .imageScale(.small)
-                    }
-                    .foregroundColor(.secondary)
+            HStack {
+                Text(name)
+                    .font(Font(name: name, size: UIFont.labelFontSize))
+                Spacer()
+                Button {
+                    UIPasteboard.general.string = name
+                } label: {
+                    Image(systemName: "doc.on.doc")
+                        .imageScale(.small)
                 }
-                if let range = fontInfo.variableInfo?.range {
-                    Slider(value: $weight, in: range, step: 1)
-                }
+                .foregroundColor(.secondary)
             }
-        }
-
-        init(fontInfo: FontInfo) {
-            self.fontInfo = fontInfo
-            self.weight = fontInfo.variableInfo?.initial ?? 0
-        }
-    }
-}
-
-@available(iOS 13.0, *)
-extension FontInfo {
-    static func allFonts() -> [FontInfo] {
-        UIFont.familyNames.flatMap { family in
-            UIFont.fontNames(forFamilyName: family)
-                .map { FontInfo(name: $0) }
-        }
-    }
-
-    static func systemFonts() -> [FontInfo] {
-        Font.Design.allCases.flatMap { design in
-            Font.Weight.allCases.map { weight in
-                FontInfo(name: "System \(design.description) \(weight.description)")
-            }
-        }
-    }
-
-    static func appFonts() -> [FontInfo] {
-        (Bundle.main.infoDictionary?["UIAppFonts"] as? [String] ?? []).compactMap {
-            if let url = Bundle.main.url(forResource: $0, withExtension: nil),
-               let fontData = try? Data(contentsOf: url),
-               let fontDataProvider = CGDataProvider(data: fontData as CFData),
-               let font = CGFont(fontDataProvider),
-               let name = font.postScriptName as? String {
-                return FontInfo(name: name, variableInfo: font.variableInfo)
-            }
-            return nil
         }
     }
 }
@@ -162,21 +131,5 @@ extension Font.Weight: CaseIterable, CustomStringConvertible {
         case .black: return "Black"
         default: return "?"
         }
-    }
-}
-
-@available(iOS 13.0, *)
-fileprivate extension CGFont {
-    var variableInfo: FontInfo.VariableInfo? {
-        guard let axes = self.variationAxes as? [[String: Any]] else { return nil }
-
-        return axes
-            .first { $0[CGFont.variationAxisName as String] as? String == "Weight" }
-            .map {
-                let min = $0[CGFont.variationAxisMinValue as String] as? Double ?? 100
-                let max = $0[CGFont.variationAxisMaxValue as String] as? Double ?? 900
-                let initial = $0[CGFont.variationAxisDefaultValue as String] as? Double ?? 400
-                return FontInfo.VariableInfo(range: min...max, initial: initial)
-            }
     }
 }
