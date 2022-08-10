0572cac8cf3d8d20e15fa6f9355932289310d9e7
Matt Hayashida <matt.hayashida@appcues.com>
2022-08-10 10:50:10 -0400

    👌 Update debugger deep link messaging

--- Sources/AppcuesKit/Presentation/Debugger/Panel/DebugViewModel.swift
+++ Sources/AppcuesKit/Presentation/Debugger/Panel/DebugViewModel.swift
@@ -34,7 +34,7 @@ internal class DebugViewModel: ObservableObject {
     }
     @Published var filter: DebugViewModel.LoggedEvent.EventType?
     @Published private(set) var connectedStatus = StatusItem(status: .pending, title: "Connected to Appcues")
-    @Published private(set) var deepLinkStatus = StatusItem(status: .pending, title: "Appcues Deep Link Configured")
+    @Published private(set) var deepLinkStatus = StatusItem(status: .pending, title: "Appcues Deep Link", subtitle: "Tap to check configuration")
     @Published private(set) var trackingPages = false
     @Published private(set) var userIdentified = false
     @Published var isAnonymous = false
