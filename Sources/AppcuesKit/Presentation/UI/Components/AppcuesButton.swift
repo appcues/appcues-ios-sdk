2f362efd16fbafe2095e4d278b54342aed5dc18f
Matt Hayashida <matt.hayashida@appcues.com>
2022-09-06 11:36:05 -0400

    🐛 Fix content alignment not applied within button views

--- Sources/AppcuesKit/Presentation/UI/Components/AppcuesButton.swift
+++ Sources/AppcuesKit/Presentation/UI/Components/AppcuesButton.swift
@@ -21,6 +21,13 @@ internal struct AppcuesButton: View {
             // handle tap in `.setupActions`
         } label: {
             model.content.view
+                // If the button has a set width instead of sizing according to the content,
+                // the content may need to be aligned within the the expanded space.
+                // The `if` check is necessary since `maxWidth: .infinity` would blow things up
+                // if there was no set width on the Button itself.
+                .if(style.width != nil || style.fillWidth) { view in
+                    view.frame(maxWidth: .infinity, alignment: Alignment(vertical: model.content.style?.verticalAlignment, horizontal: model.content.style?.horizontalAlignment) ?? .center)
+                }
                 // Applying the Button padding and frame to the label ensures the proper button highlight effect
                 // on touchDown everywhere within the button frame.
                 .applyInternalLayout(style)
