39854d1e14b77efedc648ee5729471476bd3c951
Matt Hayashida <matt.hayashida@appcues.com>
2022-09-29 11:07:24 -0400

    ♻️ Add ExperienceActionQueueTransforming to allow actions to manipulate the queue

---
+++ Sources/AppcuesKit/Presentation/Actions/ExperienceActionQueueTransforming.swift
@@ -0,0 +1,22 @@
+//
+//  ExperienceActionQueueTransforming.swift
+//  AppcuesKit
+//
+//  Created by Matt on 2022-09-28.
+//  Copyright © 2022 Appcues. All rights reserved.
+//
+
+import Foundation
+
+/// An `ExperienceAction` that performs modifications of the action queue executed following an interaction in an experience.
+@objc
+internal protocol ExperienceActionQueueTransforming: ExperienceAction {
+
+    /// Modify the queue of actions executed in an experience.
+    /// - Parameters:
+    ///   - queue: The current queue of actions.
+    ///   - index: The index of the current action in the `queue`.
+    ///   - appcues: The `Appcues` instance that displayed the experience triggering the action.
+    /// - Returns: The updated queue.
+    func transformQueue(_ queue: [ExperienceAction], index: Int, inContext appcues: Appcues) -> [ExperienceAction]
+}
