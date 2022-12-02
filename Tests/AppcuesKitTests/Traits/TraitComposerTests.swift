eaf65857a7dafe2a74c9aa2fd51aeed490e918c4
Matt Hayashida <matt.hayashida@appcues.com>
2022-12-02 15:38:32 -0500

    ✅ Update trait composition tests

--- Tests/AppcuesKitTests/Traits/TraitComposerTests.swift
+++ Tests/AppcuesKitTests/Traits/TraitComposerTests.swift
@@ -72,15 +72,11 @@ class TraitComposerTests: XCTestCase {
         traitRegistry.register(trait: TestTrait.self)
         traitRegistry.register(trait: TestPresentingTrait.self)
 
-        // `expectedFulfillmentCount = 2` are for traits that have multiple applied
         let stepDecoratingExpectation = expectation(description: "Step decorate called")
-        stepDecoratingExpectation.expectedFulfillmentCount = 2
         let containerCreatingExpectation = expectation(description: "Create container called")
         let containerDecoratingExpectation = expectation(description: "Container decorate called")
-        containerDecoratingExpectation.expectedFulfillmentCount = 2
         let wrapperCreatingExpectation = expectation(description: "Create wrapper called")
         let backdropDecoratingExpectation = expectation(description: "Backdrop decorate called")
-        backdropDecoratingExpectation.expectedFulfillmentCount = 2
 
         let experience = makeTestExperience(
             stepDecoratingExpectation: stepDecoratingExpectation,
@@ -123,23 +119,38 @@ class TraitComposerTests: XCTestCase {
 
         // Arrange
         traitRegistry.register(trait: TestTrait.self)
+        traitRegistry.register(trait: Test2Trait.self)
         traitRegistry.register(trait: TestPresentingTrait.self)
 
-        let experienceLevelStepDecoratingExpectation = expectation(description: "Step decorate called")
-        // ContainerCreating should be superceded by the more specific groupLevelContainerCreatingExpectation
-        let experienceLevelContainerCreatingExpectation = expectation(description: "Create container called")
+        // No experience level expectations from @test/trait should be applied because the group also has @test/trait
+        let experienceLevelStepDecoratingExpectation = expectation(description: "Experience level step decorate called")
+        experienceLevelStepDecoratingExpectation.isInverted = true
+        let experienceLevelContainerCreatingExpectation = expectation(description: "Experience level create container called")
         experienceLevelContainerCreatingExpectation.isInverted = true
-        let experienceLevelContainerDecoratingExpectation = expectation(description: "Container decorate called")
-        // WrapperCreating should be superceded by the more specific groupLevelWrapperCreatingExpectation
-        let experienceLevelWrapperCreatingExpectation = expectation(description: "Create wrapper called")
+        let experienceLevelContainerDecoratingExpectation = expectation(description: "Experience level container decorate called")
+        experienceLevelContainerDecoratingExpectation.isInverted = true
+        let experienceLevelWrapperCreatingExpectation = expectation(description: "Experience level create wrapper called")
         experienceLevelWrapperCreatingExpectation.isInverted = true
-        let experienceLevelBackdropDecoratingExpectation = expectation(description: "Backdrop decorate called")
+        let experienceLevelBackdropDecoratingExpectation = expectation(description: "Experience level backdrop decorate called")
+        experienceLevelBackdropDecoratingExpectation.isInverted = true
 
-        let groupLevelStepDecoratingExpectation = expectation(description: "Step decorate called")
-        let groupLevelContainerCreatingExpectation = expectation(description: "Create container called")
-        let groupLevelContainerDecoratingExpectation = expectation(description: "Container decorate called")
-        let groupLevelWrapperCreatingExpectation = expectation(description: "Create wrapper called")
-        let groupLevelBackdropDecoratingExpectation = expectation(description: "Backdrop decorate called")
+        // Some experience level expectations from @test/trait-2 should be applied when multiple traits with the same capability are allowed
+        let experienceLevel2StepDecoratingExpectation = expectation(description: "Experience level 2 step decorate called")
+        // ContainerCreating should be superceded by the more specific groupLevelContainerCreatingExpectation
+        let experienceLevel2ContainerCreatingExpectation = expectation(description: "Experience level 2 create container called")
+        experienceLevel2ContainerCreatingExpectation.isInverted = true
+        let experienceLevel2ContainerDecoratingExpectation = expectation(description: "Experience level 2 container decorate called")
+        // WrapperCreating should be superceded by the more specific groupLevelWrapperCreatingExpectation
+        let experienceLevel2WrapperCreatingExpectation = expectation(description: "Experience level 2 create wrapper called")
+        experienceLevel2WrapperCreatingExpectation.isInverted = true
+        let experienceLevel2BackdropDecoratingExpectation = expectation(description: "Experience level 2 backdrop decorate called")
+
+
+        let groupLevelStepDecoratingExpectation = expectation(description: "Group level step decorate called")
+        let groupLevelContainerCreatingExpectation = expectation(description: "Group level create container called")
+        let groupLevelContainerDecoratingExpectation = expectation(description: "Group level container decorate called")
+        let groupLevelWrapperCreatingExpectation = expectation(description: "Group level create wrapper called")
+        let groupLevelBackdropDecoratingExpectation = expectation(description: "Group level backdrop decorate called")
 
 
         let experience = Experience(
@@ -159,6 +170,15 @@ class TraitComposerTests: XCTestCase {
                         "containerDecoratingExpectation": experienceLevelContainerDecoratingExpectation as Any,
                         "wrapperCreatingExpectation": experienceLevelWrapperCreatingExpectation as Any,
                         "backdropDecoratingExpectation": experienceLevelBackdropDecoratingExpectation as Any
+                    ]),
+                Experience.Trait(
+                    type: "@test/trait-2",
+                    config: [
+                        "stepDecoratingExpectation": experienceLevel2StepDecoratingExpectation as Any,
+                        "containerCreatingExpectation": experienceLevel2ContainerCreatingExpectation as Any,
+                        "containerDecoratingExpectation": experienceLevel2ContainerDecoratingExpectation as Any,
+                        "wrapperCreatingExpectation": experienceLevel2WrapperCreatingExpectation as Any,
+                        "backdropDecoratingExpectation": experienceLevel2BackdropDecoratingExpectation as Any
                     ])
             ],
             steps: [
@@ -281,6 +301,74 @@ class TraitComposerTests: XCTestCase {
         }
     }
 
+    func testDecompose() throws {
+        // Arrange
+        let traits: [ExperienceTrait] = [
+            try XCTUnwrap(TestTrait(config: nil, level: .experience)),
+            try XCTUnwrap(TestPresentingTrait(config: nil, level: .experience))
+        ]
+
+        // Act
+        let decomposedTraits = TraitComposer.DecomposedTraits(traits: traits)
+
+        // Assert
+        XCTAssertEqual(decomposedTraits.allTraitInstances.count, 2)
+        XCTAssertEqual(decomposedTraits.stepDecorating.count, 1)
+        XCTAssertNotNil(decomposedTraits.containerCreating)
+        XCTAssertEqual(decomposedTraits.containerDecorating.count, 1)
+        XCTAssertEqual(decomposedTraits.backdropDecorating.count, 1)
+        XCTAssertNotNil(decomposedTraits.wrapperCreating)
+        XCTAssertNotNil(decomposedTraits.presenting)
+    }
+
+    func testAppendDecomposedTraits() throws {
+        // Arrange
+        let experienceTraits: [ExperienceTrait] = [
+            try XCTUnwrap(TestTrait(config: nil, level: .experience)),
+            try XCTUnwrap(TestPresentingTrait(config: nil, level: .experience))
+        ]
+        let groupTrait = try XCTUnwrap(TestTrait(config: nil, level: .experience))
+        let decomposedTraits = TraitComposer.DecomposedTraits(traits: experienceTraits)
+
+        // Act
+        decomposedTraits.append(contentsOf: TraitComposer.DecomposedTraits(traits: [groupTrait]))
+
+        // Assert
+        XCTAssertEqual(decomposedTraits.allTraitInstances.count, 3)
+        XCTAssertEqual(decomposedTraits.stepDecorating.count, 2)
+        XCTAssertTrue(decomposedTraits.containerCreating === groupTrait, "appended trait takes precedence over existing level")
+        XCTAssertEqual(decomposedTraits.containerDecorating.count, 2)
+        XCTAssertEqual(decomposedTraits.backdropDecorating.count, 2)
+        XCTAssertTrue(decomposedTraits.wrapperCreating === groupTrait, "appended trait takes precedence over existing level")
+        XCTAssertNotNil(decomposedTraits.presenting)
+    }
+
+    func testPropagateDecomposedTraits() throws {
+        // Arrange
+        let experienceTraits: [ExperienceTrait] = [
+            try XCTUnwrap(Test2Trait(config: nil, level: .experience)),
+            try XCTUnwrap(Test3Trait(config: nil, level: .group))
+        ]
+        let decomposedTraits = TraitComposer.DecomposedTraits(traits: experienceTraits)
+        let stepTrait = try XCTUnwrap(TestTrait(config: nil, level: .experience))
+        let decomposedStepTraits = TraitComposer.DecomposedTraits(traits: [stepTrait])
+
+        // Act
+        decomposedStepTraits.propagateDecorators(from: decomposedTraits)
+
+        // Assert
+        XCTAssertEqual(decomposedStepTraits.allTraitInstances.count, 1)
+        XCTAssertEqual(decomposedStepTraits.stepDecorating.count, 3)
+        XCTAssertEqual(decomposedStepTraits.stepDecorating.map({ type(of: $0).type }), ["@test/trait-2", "@test/trait-3", "@test/trait"])
+        XCTAssertNotNil(decomposedStepTraits.containerCreating)
+        XCTAssertEqual(decomposedStepTraits.containerDecorating.count, 3)
+        XCTAssertEqual(decomposedStepTraits.containerDecorating.map({ type(of: $0).type }), ["@test/trait-2", "@test/trait-3", "@test/trait"])
+        XCTAssertEqual(decomposedStepTraits.backdropDecorating.count, 3)
+        XCTAssertEqual(decomposedStepTraits.backdropDecorating.map({ type(of: $0).type }), ["@test/trait-2", "@test/trait-3", "@test/trait"])
+        XCTAssertNotNil(decomposedStepTraits.containerCreating)
+        XCTAssertNil(decomposedStepTraits.presenting)
+    }
+
     // MARK: - Helpers
 
     private func makeTestExperience(
@@ -362,8 +450,16 @@ private extension Experience.Step.Child {
 
 @available(iOS 13.0, *)
 extension TraitComposerTests {
+    class Test2Trait: TestTrait {
+        override class var type: String { "@test/trait-2" }
+    }
+
+    class Test3Trait: TestTrait {
+        override class var type: String { "@test/trait-3" }
+    }
+
     class TestTrait: StepDecoratingTrait, ContainerCreatingTrait, ContainerDecoratingTrait, WrapperCreatingTrait, BackdropDecoratingTrait {
-        static let type = "@test/trait"
+        class var type: String { "@test/trait" }
 
         let groupID: String?
 
@@ -410,6 +506,10 @@ extension TraitComposerTests {
             containerDecoratingExpectation?.fulfill()
         }
 
+        func undecorate(containerController: AppcuesKit.ExperienceContainerViewController) throws {
+            // no-op
+        }
+
         // WrapperCreatingTrait
 
         func createWrapper(around containerController: ExperienceContainerViewController) throws -> UIViewController {
@@ -425,6 +525,10 @@ extension TraitComposerTests {
 
         func decorate(backdropView: UIView) throws {
             backdropDecoratingExpectation?.fulfill()
+        }
+
+        func undecorate(backdropView: UIView) throws {
+            // no-op
         }
     }
 
