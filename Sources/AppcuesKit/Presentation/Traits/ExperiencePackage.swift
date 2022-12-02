515aa5eccc0b70b9e7bc03fc3a8426f52595fe57
Matt Hayashida <matt.hayashida@appcues.com>
2022-12-02 14:57:53 -0500

    ♻️ Report a step error when a decorating trait fails
    Moves the step container navigation notification sending responsibility to state machine from ExperienceContainerViewController instances

--- Sources/AppcuesKit/Presentation/Traits/ExperiencePackage.swift
+++ Sources/AppcuesKit/Presentation/Traits/ExperiencePackage.swift
@@ -8,27 +8,33 @@
 
 import UIKit
 
-internal struct ExperiencePackage {
+internal class ExperiencePackage {
     // References to the trait instances are held here to ensure they persist the lifetime of the experience being rendered.
     private let traitInstances: [ExperienceTrait]
+    let stepDecoratingTraitUpdater: (Int, Int) throws -> Void
     let steps: [Experience.Step.Child]
     let containerController: ExperienceContainerViewController
     let wrapperController: UIViewController
+    let pageMonitor: PageMonitor
     let presenter: (_ completion: (() -> Void)?) throws -> Void
     let dismisser: (_ completion: (() -> Void)?) -> Void
 
     internal init(
         traitInstances: [ExperienceTrait],
+        stepDecoratingTraitUpdater: @escaping (Int, Int) throws -> Void,
         steps: [Experience.Step.Child],
         containerController: ExperienceContainerViewController,
         wrapperController: UIViewController,
+        pageMonitor: PageMonitor,
         presenter: @escaping ((() -> Void)?) throws -> Void,
         dismisser: @escaping ((() -> Void)?) -> Void
     ) {
         self.traitInstances = traitInstances
+        self.stepDecoratingTraitUpdater = stepDecoratingTraitUpdater
         self.steps = steps
         self.containerController = containerController
         self.wrapperController = wrapperController
+        self.pageMonitor = pageMonitor
         self.presenter = presenter
         self.dismisser = dismisser
     }
