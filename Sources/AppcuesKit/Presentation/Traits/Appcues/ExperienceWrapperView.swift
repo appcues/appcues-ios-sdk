38f34af49611608383e98f273502766466578029
Matt Hayashida <matt.hayashida@appcues.com>
2022-12-20 09:22:32 -0500

    ✨ Add skeleton @appcues/tooltip trait

--- Sources/AppcuesKit/Presentation/Traits/Appcues/ExperienceWrapperView.swift
+++ Sources/AppcuesKit/Presentation/Traits/Appcues/ExperienceWrapperView.swift
@@ -1,5 +1,5 @@
 //
-//  DialogContainerView.swift
+//  ExperienceWrapperView.swift
 //  AppcuesKit
 //
 //  Created by James Ellis on 11/2/21.
@@ -9,9 +9,9 @@
 import UIKit
 
 @available(iOS 13.0, *)
-internal class DialogContainerView: UIView {
+internal class ExperienceWrapperView: UIView {
 
-    let dialogView: UIView = {
+    let contentWrapperView: UIView = {
         let view = UIView(frame: .zero)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.clipsToBounds = true
@@ -31,21 +31,19 @@ internal class DialogContainerView: UIView {
         }
     }
 
-    init() {
+    required init() {
         super.init(frame: .zero)
 
         backgroundColor = .clear
 
-        addSubview(dialogView)
+        addSubview(contentWrapperView)
+        positionContentWrapperView()
 
         NSLayoutConstraint.activate([
-            // ensure the dialog can't exceed the container height (it should scroll instead).
-            dialogView.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor),
-            dialogView.bottomAnchor.constraint(lessThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor),
             // this is required so the dialogView has an initial non-zero height, after which it can start sizing to the content.
-            dialogView.layoutMarginsGuide.bottomAnchor.constraint(greaterThanOrEqualTo: dialogView.layoutMarginsGuide.topAnchor, constant: 1),
-            dialogView.leadingAnchor.constraint(equalTo: readableContentGuide.leadingAnchor),
-            dialogView.trailingAnchor.constraint(equalTo: readableContentGuide.trailingAnchor)
+            contentWrapperView.layoutMarginsGuide.bottomAnchor.constraint(
+                greaterThanOrEqualTo: contentWrapperView.layoutMarginsGuide.topAnchor,
+                constant: 1)
         ])
     }
 
@@ -58,8 +56,22 @@ internal class DialogContainerView: UIView {
         super.layoutSubviews()
 
         if let shadowLayer = shadowLayer {
-            shadowLayer.path = UIBezierPath(roundedRect: dialogView.frame, cornerRadius: dialogView.layer.cornerRadius).cgPath
+            shadowLayer.path = UIBezierPath(
+                roundedRect: contentWrapperView.frame,
+                cornerRadius: contentWrapperView.layer.cornerRadius
+            ).cgPath
             shadowLayer.shadowPath = shadowLayer.path
         }
+    }
+
+    func positionContentWrapperView() {
+        NSLayoutConstraint.activate([
+            contentWrapperView.leadingAnchor.constraint(equalTo: readableContentGuide.leadingAnchor),
+            contentWrapperView.trailingAnchor.constraint(equalTo: readableContentGuide.trailingAnchor),
+
+            // ensure the dialog can't exceed the container height (it should scroll instead).
+            contentWrapperView.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor),
+            contentWrapperView.bottomAnchor.constraint(lessThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 }
