84c0c3190887bef44e84216e3e7fd90100c692ed
Matt Hayashida <matt.hayashida@appcues.com>
2023-01-05 10:50:24 -0500

    ✨ Add support for multiple text styles in a single primitive

--- Sources/AppcuesKit/Presentation/UI/Components/AppcuesText.swift
+++ Sources/AppcuesKit/Presentation/UI/Components/AppcuesText.swift
@@ -17,10 +17,44 @@ internal struct AppcuesText: View {
     var body: some View {
         let style = AppcuesStyle(from: model.style)
 
-        Text(model.text)
+        Text(textModel: model)
             .applyTextStyle(style, model: model)
             .setupActions(on: viewModel, for: model)
             .applyAllAppcues(style)
             .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+@available(iOS 13.0, *)
+extension Text {
+    init(textModel: ExperienceComponent.TextModel) {
+        if let spans = textModel.spans {
+            self.init("")
+
+            // Note: a ViewBuilder approach here doesn't work because the requirement that we operate strictly on `Text`
+            // and not `some View` for concatenation to work. Therefore we work with the struct directly.
+            spans.forEach { span in
+                let style = AppcuesStyle(from: span.style)
+                var text = Text(span.text)
+
+                if let font = style.font {
+                    text = text.font(font)
+                }
+
+                if let foregroundColor = style.foregroundColor {
+                    text = text.foregroundColor(foregroundColor)
+                }
+
+                if let kerning = style.letterSpacing {
+                    text = text.kerning(kerning)
+                }
+
+                // A shorthand operator with `Text` doesn't compile
+                // swiftlint:disable:next shorthand_operator
+                self = self + text
+            }
+        } else {
+            self.init(textModel.text)
+        }
     }
 }
