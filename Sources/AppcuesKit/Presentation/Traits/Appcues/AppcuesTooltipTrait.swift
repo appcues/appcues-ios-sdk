08c42a27bdc23e5f57a874d0b33cfa89bfd837c1
Matt Hayashida <matt.hayashida@appcues.com>
2023-01-09 15:34:30 -0500

    ✨ Add skeleton @appcues/tooltip trait
    1

--- Sources/AppcuesKit/Presentation/Traits/Appcues/AppcuesTooltipTrait.swift
+++ Sources/AppcuesKit/Presentation/Traits/Appcues/AppcuesTooltipTrait.swift
@@ -11,6 +11,11 @@ import UIKit
 @available(iOS 13.0, *)
 internal class AppcuesTooltipTrait: StepDecoratingTrait, WrapperCreatingTrait, PresentingTrait {
     struct Config: Decodable {
+        let preferredPosition: TooltipPosition?
+        let hidePointer: Bool?
+        let pointerBase: Double?
+        let pointerLength: Double?
+        let distanceFromTarget: Double?
         let style: ExperienceComponent.Style?
     }
 
@@ -19,10 +24,18 @@ internal class AppcuesTooltipTrait: StepDecoratingTrait, WrapperCreatingTrait, P
     weak var metadataDelegate: TraitMetadataDelegate?
 
     let tooltipStyle: ExperienceComponent.Style?
+    let preferredPosition: TooltipPosition?
+    let hidePointer: Bool
+    let pointerSize: CGSize
+    let distanceFromTarget: CGFloat
 
     required init?(configuration: ExperiencePluginConfiguration, level: ExperienceTraitLevel) {
         guard let config = configuration.decode(Config.self) else { return nil }
+        self.preferredPosition = config.preferredPosition
+        self.hidePointer = config.hidePointer ?? false
+        self.pointerSize = CGSize(width: config.pointerBase ?? 16, height: config.pointerLength ?? 8)
         self.tooltipStyle = config.style
+        self.distanceFromTarget = config.distanceFromTarget ?? 0
     }
 
     func decorate(stepController: UIViewController) throws {
@@ -38,14 +51,19 @@ internal class AppcuesTooltipTrait: StepDecoratingTrait, WrapperCreatingTrait, P
     func createWrapper(around containerController: ExperienceContainerViewController) throws -> UIViewController {
         let dialogWrapperViewController = ExperienceWrapperViewController<TooltipWrapperView>(wrapping: containerController)
         dialogWrapperViewController.configureStyle(tooltipStyle)
-
-        metadataDelegate?.registerHandler(for: Self.type, animating: false) { metadata in
-            // layout everything before adjusting the position so that only the position animates when another layout pass runs
-            dialogWrapperViewController.bodyView.layoutIfNeeded()
-            dialogWrapperViewController.bodyView.targetRectangle = metadata["targetRectangle"]
+        dialogWrapperViewController.bodyView.preferredPosition = preferredPosition
+        dialogWrapperViewController.bodyView.pointerSize = hidePointer ? nil : pointerSize
+        dialogWrapperViewController.bodyView.distanceFromTarget = distanceFromTarget
+        if let preferredWidth = tooltipStyle?.width {
+            dialogWrapperViewController.bodyView.preferredWidth = preferredWidth
         }
-        metadataDelegate?.registerHandler(for: Self.type, animating: true) { _ in
-            dialogWrapperViewController.bodyView.layoutIfNeeded()
+        // Ensure content isnt hidden by the tooltip pointer
+        dialogWrapperViewController.bodyView.pointerInsetHandler = { insets in
+            containerController.additionalSafeAreaInsets = insets
+        }
+
+        metadataDelegate?.registerHandler(for: Self.type, animating: true) { metadata in
+            dialogWrapperViewController.bodyView.targetRectangle = metadata["targetRectangle"]
         }
 
         return dialogWrapperViewController
@@ -66,5 +84,15 @@ internal class AppcuesTooltipTrait: StepDecoratingTrait, WrapperCreatingTrait, P
 
     func remove(viewController: UIViewController, completion: (() -> Void)?) {
         viewController.dismiss(animated: true, completion: completion)
+    }
+}
+
+@available(iOS 13.0, *)
+extension AppcuesTooltipTrait {
+    enum TooltipPosition: String, Decodable {
+        case top
+        case bottom
+        case leading
+        case trailing
     }
 }
