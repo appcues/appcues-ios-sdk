3e946075722c3799c5ef6f07294fa39ebb6edde2
James Ellis <james@appcues.com>
2023-03-08 13:06:46 -0500

    ✨ Add success and failure toast implementations for screen capture using simplified UIView

---
+++ Sources/AppcuesKit/Presentation/Debugger/ScreenCapturing/CaptureToastView.swift
@@ -0,0 +1,103 @@
+//
+//  CaptureToastView.swift
+//  AppcuesKit
+//
+//  Created by James Ellis on 3/8/23.
+//  Copyright © 2023 Appcues. All rights reserved.
+//
+
+import UIKit
+
+internal class CaptureToastView: UIView {
+
+    var onRetry: (() -> Void)?
+
+    private var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .center
+        view.spacing = 8.0
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private var messageLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+
+    private var retryButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Try again", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.cornerRadius = 6.0
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        layer.cornerRadius = 6.0
+
+        stackView.addArrangedSubview(messageLabel)
+        stackView.addArrangedSubview(retryButton)
+
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
+
+            retryButton.heightAnchor.constraint(equalToConstant: 40),
+            retryButton.widthAnchor.constraint(equalToConstant: 90)
+        ])
+
+        retryButton.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureSuccess(_ capture: Capture) {
+        backgroundColor = .appcuesToastSuccess
+        retryButton.isHidden = true
+        var message = NSMutableAttributedString(string: "\"\(capture.displayName)\"", attributes: [
+            .font: UIFont.systemFont(ofSize: 14, weight: .bold),
+            .foregroundColor: UIColor.white
+        ])
+        message.append(NSAttributedString(string: " is now available for preview and targeting.", attributes: [
+            .font: UIFont.systemFont(ofSize: 14, weight: .regular),
+            .foregroundColor: UIColor.white
+        ]))
+        messageLabel.attributedText = message
+        onRetry = nil
+    }
+
+    func configureFailure(onRetry: @escaping () -> Void) {
+        backgroundColor = .appcuesToastFailure
+        retryButton.isHidden = false
+        messageLabel.attributedText = NSAttributedString(string: "Upload failed", attributes: [
+            .font: UIFont.systemFont(ofSize: 14, weight: .bold),
+            .foregroundColor: UIColor.white
+        ])
+        self.onRetry = onRetry
+    }
+
+    @objc
+    func retryButtonTapped() {
+        onRetry?()
+    }
+}
+
+private extension UIColor {
+    static let appcuesToastSuccess = UIColor(red: 0.0, green: 0.447, blue: 0.839, alpha: 1.0)
+    static let appcuesToastFailure = UIColor(red: 0.867, green: 0.133, blue: 0.439, alpha: 1.0)
+}
