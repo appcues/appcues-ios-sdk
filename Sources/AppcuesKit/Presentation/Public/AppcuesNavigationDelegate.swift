465da9d25bd2389b15020e63eba669b749459ba3
Matt Hayashida <matt.hayashida@appcues.com>
2023-03-22 11:27:08 -0400

    📝 Add navigation docs

--- Sources/AppcuesKit/Presentation/Public/AppcuesNavigationDelegate.swift
+++ Sources/AppcuesKit/Presentation/Public/AppcuesNavigationDelegate.swift
@@ -14,6 +14,7 @@ public protocol AppcuesNavigationDelegate: AnyObject {
     /// Requests the delegate navigate to the given destination, and report completion.
     /// - Parameters:
     ///   - url: The URL of the destination to navigate.
+    ///   - openExternally: `true` when the link is intended to open in the default web browser, `false` when an in-app browser.
     ///   - completion: Closure to invoke when navigation is completed, passing `true` if successfully navigated, `false` if not.
     func navigate(to url: URL, openExternally: Bool, completion: @escaping (Bool) -> Void)
 }
