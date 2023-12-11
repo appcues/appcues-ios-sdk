9fb9ca5874ec3f1e0ced7a4a10adbcc6b7afe945
Matt Hayashida <matt.hayashida@appcues.com>
2023-12-11 11:20:00 -0500

    🎬 Add scrolling content to example apps
    Contains targetable buttons, button1…button5

---
+++ Examples/DeveloperSPMExample/SPMExample/ScrollingTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollingTableViewController.swift
+//  AppcuesSPMExample
+//
+//  Created by Matt on 2023-12-11.
+//
+
+import UIKit
+import AppcuesKit
+
+class ScrollingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Appcues.shared.screen(title: "Scrolling Table")
+    }
+}
