2feef4c159ae2f2a8c2eaf2fd1588b9e964a1734
Matt Hayashida <matt.hayashida@appcues.com>
2023-12-15 11:07:02 -0500

    🐛 Fix crash from lack of thread safety in DIContainer

--- Sources/AppcuesKit/Utilities/DIContainer.swift
+++ Sources/AppcuesKit/Utilities/DIContainer.swift
@@ -15,6 +15,8 @@ internal class DIContainer {
     // reference:
     // https://github.com/Swinject/SwinjectAutoregistration/blob/master/Sources/AutoRegistration.swift
 
+    private let componentQueue = DispatchQueue(label: "appcues-dicontainer", qos: .userInitiated, attributes: .concurrent)
+
     private var initializers: [String: (DIContainer) -> Any] = [:]
     private var components: [String: Any] = [:]
 
@@ -29,20 +31,28 @@ internal class DIContainer {
     }
 
     func register<Component>(_ type: Component.Type, value: Component) {
-        components[String(describing: Component.self)] = value
+        // If we're writing, need to lock, but no return, so this can be async
+        componentQueue.async(flags: .barrier) {
+            self.components[String(describing: Component.self)] = value
+        }
     }
 
     @discardableResult
     func resolve<Component>(_ type: Component.Type) -> Component {
         let key = String(describing: Component.self)
-        if let component = components[key] as? Component {
+
+        // Concurrent reads are ok
+        if let component = componentQueue.sync(execute: { components[key] as? Component }) {
             return component
         }
 
         if let initializer = initializers[key] {
             // swiftlint:disable:next force_cast
             let component = initializer(self) as! Component
-            components[key] = component
+            // If we're writing, need to lock
+            componentQueue.sync(flags: .barrier) {
+                components[key] = component
+            }
             return component
         }
 
