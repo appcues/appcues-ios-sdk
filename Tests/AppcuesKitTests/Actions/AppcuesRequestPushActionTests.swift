bdd8e441ebc0b31a0bf9f9224a98cf22d9a32c08
Matt Hayashida <matt.hayashida@appcues.com>
2024-02-27 14:52:30 -0500

    ✅ Add push primer tests

---
+++ Tests/AppcuesKitTests/Actions/AppcuesRequestPushActionTests.swift
@@ -0,0 +1,67 @@
+//
+//  AppcuesRequestPushActionTests.swift
+//  AppcuesKitTests
+//
+//  Created by Matt on 2024-02-27.
+//  Copyright © 2024 Appcues. All rights reserved.
+//
+
+import XCTest
+@testable import AppcuesKit
+
+@available(iOS 13.0, *)
+class AppcuesRequestPushActionTests: XCTestCase {
+
+    var appcues: MockAppcues!
+
+    override func setUpWithError() throws {
+        appcues = MockAppcues()
+    }
+
+    func testInit() throws {
+        // Act
+        let action = AppcuesRequestPushAction(configuration: AppcuesExperiencePluginConfiguration(nil, appcues: appcues))
+
+        // Assert
+        XCTAssertEqual(AppcuesRequestPushAction.type, "@appcues/request-push")
+        XCTAssertNotNil(action)
+    }
+
+    func testExecute() throws {
+        // Arrange
+        var eventCount = 0
+        var completionCount = 0
+        let action = AppcuesRequestPushAction(appcues: appcues)
+
+        appcues.analyticsPublisher.onPublish = { update in
+            XCTAssertEqual(update.type, .event(name: Events.Device.deviceUpdated.rawValue, interactive: false))
+            eventCount += 1
+        }
+
+        // Act
+        action?.execute(completion: { completionCount += 1 })
+
+        // Assert
+        XCTAssertEqual(eventCount, 1)
+        XCTAssertEqual(completionCount, 1)
+    }
+
+    func testExecuteCompletesWithoutAppcuesInstance() throws {
+        // Arrange
+        var completionCount = 0
+        let action = try XCTUnwrap(AppcuesRequestPushAction(appcues: nil))
+
+        // Act
+        action.execute(completion: { completionCount += 1 })
+
+        // Assert
+        XCTAssertEqual(completionCount, 1)
+    }
+}
+
+@available(iOS 13.0, *)
+extension AppcuesRequestPushAction {
+    convenience init?(appcues: Appcues?) {
+        self.init(configuration: AppcuesExperiencePluginConfiguration(nil, appcues: appcues))
+    }
+}
