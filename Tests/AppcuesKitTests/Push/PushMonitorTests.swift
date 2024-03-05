c21eee3c92f8bb995da2534e04c664e5e5129388
Matt Hayashida <matt.hayashida@appcues.com>
2024-03-05 16:31:10 -0500

    ✅ Add push handler tests

--- Tests/AppcuesKitTests/Push/PushMonitorTests.swift
+++ Tests/AppcuesKitTests/Push/PushMonitorTests.swift
@@ -74,4 +74,201 @@ class PushMonitorTests: XCTestCase {
         XCTAssertTrue(pushMonitor.pushPrimerEligible)
     }
 
+    // MARK: Receive Handler
+
+    func testReceiveActiveSession() throws {
+        // Arrange
+        let userInfo = Dictionary<AnyHashable, Any>.appcuesPush
+
+        let analyticsExpectation = expectation(description: "push open event")
+        let completionExpectation = expectation(description: "completion called")
+
+        let completion = {
+            completionExpectation.fulfill()
+        }
+
+        appcues.analyticsPublisher.onPublish = { update in
+            XCTAssertEqual(update.type, .event(name: Events.Push.pushOpened.rawValue, interactive: false))
+            analyticsExpectation.fulfill()
+        }
+
+        appcues.storage.userID = "default-00000"
+        appcues.sessionID = UUID()
+
+        // Act
+        let result = pushMonitor.didReceiveNotification(userInfo: userInfo, completionHandler: completion)
+
+        // Assert
+        waitForExpectations(timeout: 1.0)
+        XCTAssertTrue(result)
+    }
+
+    func testReceiveActions() throws {
+        // Arrange
+        var userInfo = Dictionary<AnyHashable, Any>.appcuesPush
+        userInfo["appcues_deep_link_url"] = "app://some-link"
+        userInfo["appcues_experience_id"] = "<some-experience>"
+
+        let analyticsExpectation = expectation(description: "push open event")
+        let linkCompletionExpectation = expectation(description: "link opened")
+        let loadCompletionExpectation = expectation(description: "experience loaded")
+        let completionExpectation = expectation(description: "completion called")
+
+        appcues.analyticsPublisher.onPublish = { update in
+            XCTAssertEqual(update.type, .event(name: Events.Push.pushOpened.rawValue, interactive: false))
+            analyticsExpectation.fulfill()
+        }
+
+        let navigationDelegate = MockNavigationDelegate()
+        appcues.navigationDelegate = navigationDelegate
+        navigationDelegate.onNavigate = { url, external in
+            XCTAssertEqual(url.absoluteString, "app://some-link")
+            linkCompletionExpectation.fulfill()
+        }
+
+        appcues.experienceLoader.onLoad = { experienceID, published, trigger, completion in
+            XCTAssertEqual(experienceID, "<some-experience>")
+            XCTAssertTrue(published)
+            XCTAssertEqual(trigger, .push)
+            loadCompletionExpectation.fulfill()
+            completion?(.success(()))
+        }
+
+        let completion = {
+            completionExpectation.fulfill()
+        }
+
+        appcues.storage.userID = "default-00000"
+        appcues.sessionID = UUID()
+
+        // Act
+        let result = pushMonitor.didReceiveNotification(userInfo: userInfo, completionHandler: completion)
+
+        // Assert
+        waitForExpectations(timeout: 1.0)
+        XCTAssertTrue(result)
+    }
+
+    func testReceiveMalformed() throws {
+        // Arrange
+        let userInfo = Dictionary<AnyHashable, Any>.basicPush
+
+        let completion = {
+            XCTFail("completion should not be called")
+        }
+
+        // Act
+        let result = pushMonitor.didReceiveNotification(userInfo: userInfo, completionHandler: completion)
+
+        // Assert
+        XCTAssertFalse(result)
+    }
+
+    func testReceiveNoAppcues() throws {
+        // Arrange
+        let userInfo = Dictionary<AnyHashable, Any>.appcuesPush
+
+        let completion = {
+            XCTFail("completion should not be called")
+        }
+        appcues = nil
+
+        // Act
+        let result = pushMonitor.didReceiveNotification(userInfo: userInfo, completionHandler: completion)
+
+        // Assert
+        XCTAssertFalse(result)
+    }
+
+    func testReceiveUserIdMismatch() throws {
+        // Arrange
+        let userInfo = Dictionary<AnyHashable, Any>.appcuesPush
+
+        let completionExpectation = expectation(description: "completion called")
+
+        let completion = {
+            completionExpectation.fulfill()
+        }
+
+        appcues.analyticsPublisher.onPublish = { update in
+            XCTFail("no push opened analytic expected")
+        }
+
+        appcues.storage.userID = "some-user"
+        appcues.sessionID = UUID()
+
+        // Act
+        let result = pushMonitor.didReceiveNotification(userInfo: userInfo, completionHandler: completion)
+
+        // Assert
+        waitForExpectations(timeout: 1.0)
+        XCTAssertTrue(result)
+    }
+
+    func testReceiveNoSession() throws {
+        // Arrange
+        let userInfo = Dictionary<AnyHashable, Any>.appcuesPush
+
+        let analyticsExpectation = expectation(description: "push open event")
+        let completionExpectation = expectation(description: "completion called")
+
+        appcues.analyticsPublisher.onPublish = { update in
+            XCTAssertEqual(update.type, .event(name: Events.Push.pushOpened.rawValue, interactive: false))
+            analyticsExpectation.fulfill()
+        }
+
+        let completion = {
+            completionExpectation.fulfill()
+        }
+
+        appcues.storage.userID = "default-00000"
+        appcues.sessionID = nil
+
+        // Act
+        let result = pushMonitor.didReceiveNotification(userInfo: userInfo, completionHandler: completion)
+
+        // Assert
+        waitForExpectations(timeout: 1.0)
+        XCTAssertTrue(result)
+    }
+
+
+}
+
+extension Dictionary where Key == AnyHashable, Value == Any {
+    static var basicPush: Self {
+        [
+            "aps": [
+                "alert": [
+                    "title": "Hello world",
+                    "body": "Notification from appcues"
+                ]
+            ]
+        ]
+    }
+
+    static var appcuesPush: Self {
+        [
+            "aps": [
+                "alert": [
+                    "title": "Hello world",
+                    "body": "Notification from appcues"
+                ]
+            ],
+            "appcues_account_id": "103523",
+            "appcues_user_id": "default-00000",
+            "appcues_notification_id": "DEADBEEF-0000-0000-0000-000000000001",
+            "appcues_workflow_id": "DEADBEEF-0000-0000-0000-000000000002",
+            "appcues_workflow_task_id": "DEADBEEF-0000-0000-0000-000000000003",
+            "appcues_transaction_id": "DEADBEEF-0000-0000-0000-000000000004"
+        ]
+    }
+}
+
+private class MockNavigationDelegate: AppcuesNavigationDelegate {
+    var onNavigate: ((URL, Bool) -> Void)?
+    func navigate(to url: URL, openExternally: Bool, completion: @escaping (Bool) -> Void) {
+        onNavigate?(url, openExternally)
+        completion(true)
+    }
 }
