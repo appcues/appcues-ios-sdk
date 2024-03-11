a12bf3ae2408de90bdc28f518ce4153fa9ae6a47
Matt Hayashida <matt.hayashida@appcues.com>
2024-03-11 14:38:03 -0400

    🎬 Add cocoapods notification service extension

---
+++ Examples/DeveloperCocoapodsExample/NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,10 @@
+//
+//  NotificationService.swift
+//  NotificationServiceExtension
+//
+//  Created by James Ellis on 12/21/23.
+//
+
+import AppcuesNotificationService
+
+class NotificationService: AppcuesNotificationServiceExtension {}
