64782c425d60925ffb8cbc062c073e57fb7086f4
Matt Hayashida <matt.hayashida@appcues.com>
2024-06-05 12:06:08 -0400

    ✅ Add tests for @appcues/delay action

---
+++ Tests/AppcuesKitTests/Actions/AppcuesDelayActionTests.swift
@@ -0,0 +1,48 @@
+//
+//  AppcuesDelayActionTests.swift
+//  AppcuesKitTests
+//
+//  Created by Matt on 2024-06-05.
+//  Copyright © 2024 Appcues. All rights reserved.
+//
+
+import XCTest
+@testable import AppcuesKit
+
+@available(iOS 13.0, *)
+class AppcuesDelayActionTests: XCTestCase {
+
+    func testInit() throws {
+        // Act
+        let action = AppcuesDelayAction(appcues: nil, duration: 500)
+        let failedAction = AppcuesUpdateProfileAction(configuration: AppcuesExperiencePluginConfiguration(nil, appcues: nil))
+
+        // Assert
+        XCTAssertEqual(AppcuesDelayAction.type, "@appcues/delay")
+        XCTAssertNotNil(action)
+        XCTAssertEqual(action?.duration, 0.5)
+        XCTAssertNil(failedAction)
+    }
+
+    func testExecute() throws {
+        // Arrange
+        let completionExpectation = expectation(description: "Completion called")
+        let action = AppcuesDelayAction(duration: 0.5)
+
+        // Act
+        action.execute(completion: { completionExpectation.fulfill() })
+
+        // Assert
+        waitForExpectations(timeout: 1)
+    }
+}
+
+@available(iOS 13.0, *)
+extension AppcuesDelayAction {
+    convenience init?(appcues: Appcues?) {
+        self.init(configuration: AppcuesExperiencePluginConfiguration(nil, appcues: appcues))
+    }
+    convenience init?(appcues: Appcues?, duration: Int) {
+        self.init(configuration: AppcuesExperiencePluginConfiguration(AppcuesDelayAction.Config(duration: duration), appcues: appcues))
+    }
+}
