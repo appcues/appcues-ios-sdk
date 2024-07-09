58ec59d50c8effc2082a5be57aa6b554888723ea
Matt Hayashida <matt.hayashida@appcues.com>
2024-07-09 13:09:43 -0400

    📝 Add push notification docs

---
+++ Sources/AppcuesKit/AppcuesKit.docc/Tutorials/PushManually-02-03.swift
@@ -0,0 +1,24 @@
+import UIKit
+import AppcuesKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        application.registerForRemoteNotifications()
+
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Appcues.shared.setPushToken(deviceToken)
+    }
+
+}
+
+extension Appcues {
+    // Find your Appcues account ID in your account settings in Appcues Studio.
+    // Find your Appcues application ID in your account settings under the Apps & Installation tab in Appcues Studio.
+    static var shared = Appcues(config: Config(accountID: <#APPCUES_ACCOUNT_ID#>, applicationID: <#APPCUES_APPLICATION_ID#>))
+}
