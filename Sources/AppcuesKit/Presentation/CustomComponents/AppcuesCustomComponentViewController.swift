4a908370ab2ece4c93fa86736a079b7ce9205219
Matt Hayashida <matt.hayashida@appcues.com>
2024-11-06 11:14:02 -0500

    🚨 Fix lint warnings

--- Sources/AppcuesKit/Presentation/CustomComponents/AppcuesCustomComponentViewController.swift
+++ Sources/AppcuesKit/Presentation/CustomComponents/AppcuesCustomComponentViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 /// A protocol that a  `UIViewController` must adopt to allow usage as an Appcues custom component.
 public protocol AppcuesCustomComponentViewController: UIViewController {
-    
+
+    /// Optional component configuration for testing in the Appcues Mobile Debugger.
+    /// Refer to <doc:CustomComponentConfiguring>.
+    static var debugConfig: [String: Any]? { get }
+
     /// Creates a view controller for use as an Appcues custom component.
     /// - Parameters:
     ///   - configuration: Configuration object that decodes instances of a plugin configuration from an Experience JSON model.
     ///   - actionController: Action options for a custom component to invoke.
     init?(configuration: AppcuesExperiencePluginConfiguration, actionController: AppcuesExperienceActions)
-
-    /// Optional component configuration for testing in the Appcues Mobile Debugger.
-    /// Refer to <doc:CustomComponentConfiguring>.
-    static var debugConfig: [String: Any]? { get }
 }
 
 // Default value to make `debugConfig` optional
